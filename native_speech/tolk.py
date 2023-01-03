67b820c7fce764c0ba047663ff09c0b216c14ddf
blind.lightning.productions <blind.lightning.productions@gmail.com>
2023-01-03 21:33:34 +0000

    fix: windows tolk support.

--- native_speech/tolk.py
+++ native_speech/tolk.py
@@ -1,27 +1,26 @@
-import cytolk
+from cytolk import tolk
 
 from .base_speaker import BaseSpeaker
 
 class Speaker(BaseSpeaker):
     def __init__(self, *, try_sapi: bool = True, prefer_sapi: bool = False, tolk_extended_search_path: bool = None, **kwargs):
-        cytolk.try_sapi(try_sapi)
-        cytolk.prefer_sapi(prefer_sapi)
+        tolk.try_sapi(try_sapi)
+        tolk.prefer_sapi(prefer_sapi)
 
-        if tolk_extended_search_path is None: tolk_extended_search_path = "__compiled__" in globals()
-        if not cytolk.load(tolk_extended_search_path):
-            raise RuntimeError("Could not initialize cytolk")
+        if tolk_extended_search_path is None: tolk_extended_search_path = "__compiled__" not in globals()
+        tolk.load(tolk_extended_search_path)
 
     def __del__(self):
-        cytolk.unload()
+        tolk.unload()
 
     def speak(self, text: str, interrupt: bool = True):
-        cytolk.output(text, interrupt)
+        tolk.output(text, interrupt)
 
     def speak_char(self, character: str, interrupt: bool = True):
-        cytolk.output(character, interrupt)
+        tolk.output(character, interrupt)
 
     def speak_key(self, key: str, interrupt: bool = True):
-        cytolk.output(key, interrupt)
+        tolk.output(key, interrupt)
 
     def stop(self):
-        cytolk.silence()
+        tolk.silence()
